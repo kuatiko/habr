fad6374f92dde965464c1a84eb18ccb853057920
Kuat Mussabekov <>
2022-10-14 16:50:28 +0000

    Initial commit

---
+++ main.swift
@@ -0,0 +1,69 @@
+func exercise() {
+
+    var a = 5
+    var b = 8
+    // 🚨 Don't change the code above 👆
+
+    //####################################
+    //Write your code below this line 👇
+    var c = b
+    b = a
+    a = c
+
+
+
+    //Write your code above this line 👆
+    //####################################
+
+    //🚨 Don't change the code below 👇
+    print("a: \(a)")
+    print("b: \(b)")
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Don't change this.
+//You don't need this in the test.
+exercise()
